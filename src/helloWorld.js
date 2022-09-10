a306e85df0a27e837b0a41674c4d84c525d09992
Abhishek Kumar <Abhishek.kumar4@walmart.com>
2022-09-10 14:19:30 +0530

    feat: create minimum configuration webpack file

---
+++ src/helloWorld.js
@@ -0,0 +1,5 @@
+function helloWorld() {
+  console.log("this is hello world");
+}
+
+export { helloWorld };
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // this is the entry point, this file usually imports all other modules in your application
+  entry: "./src/index.js", // webpack will start from this file when running the build process.
+
+  // here we can specify a name of the file which will be generated as a result of the build process
+  // pass to the directory where this file should be generated
+  output: {
+    filename: "bundle.js",
+    path: "./dist", // the output dir webpack will automatically generate this.
+  },
+  mode: "none",
+};
